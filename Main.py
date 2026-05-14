3359f96dc0dc638f9b2dc3a7907cfb96bc4e5662
LUCAS FREITAS VALERIANO PEREIRA <14338057661@pedag.local>
2026-05-14 15:28:04 -0300

    Revert CORS to allow all origins for testing

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ import os
 from datetime import datetime
 
 app = Flask(__name__)
-CORS(app, origins=["https://lukz-dev.github.io"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+CORS(app, origins="*", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
      allow_headers=["*"])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///goiabal.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
